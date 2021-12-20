435f27a101160f8f9a7eb1f0f7760f6592c1aa0e
samuelgsgs <tabletsamugs@gmail.com>
2021-12-20 14:28:06 +0100

    Cambios 20-12 finalizada clase

--- src/components/Lista.jsx
+++ src/components/Lista.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import ComponenteListaClase from './ComponenteListaClase';
 export function Lista(props) {
-  const listaComponentes = [];
-
+  const listaInicial = [];
   if (props.elementos !== undefined) {
     for (let i = 0; i < props.elementos.length; i++) {
-      listaComponentes.push(
+      listaInicial.push(
         <ComponenteListaClase
           done={props.elementos[i].done}
           texto={props.elementos[i].texto}
@@ -15,10 +14,40 @@ export function Lista(props) {
     }
   }
 
+  const [listaComponentes, setListaComponentes] = useState(listaInicial);
+  const valorTextInput = useRef('');
+  const valorPrioritySelect = useRef('');
+
+  const funcion = function addElement() {
+    const newLista = listaComponentes.concat(
+      <ComponenteListaClase
+        texto={valorTextInput.current.value}
+        prioridad={valorPrioritySelect.current.value}
+      />
+    );
+    setListaComponentes(newLista);
+  };
+
   return (
     <div>
       {props.titulo} - {props.icono}
-      <ul>{listaComponentes}</ul>
+      <ul>
+        {listaComponentes}
+        <li>
+          <input
+            ref={valorTextInput}
+            type="text"
+            placeholder="Introduce una tarea"
+          />
+          <br />
+          <select ref={valorPrioritySelect}>
+            <option value="alta">Prioridad Alta</option>
+            <option value="media">Prioridad Media</option>
+            <option value="baja">Prioridad Baja</option>
+          </select>
+          <button onClick={funcion}>Añadir</button>
+        </li>
+      </ul>
     </div>
   );
 }

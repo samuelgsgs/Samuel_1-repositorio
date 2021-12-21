e0bac8c0b1abc44526873a948b42f84fdbd35bea
samuelgsgs <tabletsamugs@gmail.com>
2021-12-21 16:18:32 +0100

    Práctica 1: crear componente ListaClase

--- src/App.jsx
+++ src/App.jsx
@@ -14,13 +14,36 @@ export function App() {
     { done: false, texto: 'Leche' },
   ];
   const elementos3 = [{ done: false, texto: 'Estudiar React.js' }];
+  const elementos4 = [
+    { done: false, texto: 'Carter', prioridad: 'alta' },
+    { done: true, texto: 'Linea Escapes', prioridad: 'media' },
+    { done: true, texto: 'Ruedas', prioridad: 'alta' },
+    { done: false, texto: 'Tapones Antirrobo', prioridad: 'baja' },
+  ];
+  const elementos5 = [
+    { done: true, texto: 'Insignia', prioridad: 'baja' },
+    { done: true, texto: 'Carter', prioridad: 'alta' },
+    { done: false, texto: 'Linea Escapes', prioridad: 'media' },
+    { done: true, texto: 'Tapones Antirrobo', prioridad: 'baja' },
+  ];
+  const elementos6 = [
+    { done: true, texto: 'Carter', prioridad: 'alta' },
+    { done: false, texto: 'Linea Escapes', prioridad: 'media' },
+    { done: false, texto: 'Tapones Antirrobo', prioridad: 'baja' },
+    { done: true, texto: 'Parasol', prioridad: 'baja' },
+  ];
 
   return (
     <div>
+      <h1>Piezas del Coche</h1>
+      <ListaClase titulo="Comprar BMW" icono="🚗" elementos={elementos4} />
+      <ListaClase titulo="Comprar Mercedes" icono="🏎️" elementos={elementos5} />
+      <ListaClase titulo="Comparar Audi" icono="🏆" elementos={elementos6} />
+
       <h1>Listas de tareas</h1>
       <ListaClase titulo="Tareas de la casa" icono="♥" elementos={elementos1} />
       <Lista titulo="Lista de la compra" icono="✌" elementos={elementos2} />
-      <Lista titulo="ToDos del curso" icono="🙌" elementos={elementos3} />
+      <Lista titulo="Todos del curso" icono="🙌" elementos={elementos3} />
     </div>
   );
 }
--- src/components/ListaClase.jsx
+++ src/components/ListaClase.jsx
@@ -4,13 +4,58 @@ import ComponenteListaClase from './ComponenteListaClase';
 class ListaClase extends React.Component {
   constructor(props) {
     super(props);
-    this.icono = props.icono;
     this.titulo = props.titulo;
+    this.icono = props.icono;
+    this.elementos = props.elementos;
+    this.listainicio = [];
+
+    if (this.elementos !== undefined) {
+      for (let i = 0; i < this.elementos.length; i++) {
+        this.listainicio.push(
+          <ComponenteListaClase
+            done={this.elementos[i].done}
+            texto={this.elementos[i].texto}
+            prioridad={this.elementos[i].prioridad}
+          />
+        );
+      }
+    }
+    this.state = { listacomps: this.listainicio };
+    this.valorTextInput = React.createRef();
+    this.valorPrioritySelect = React.createRef();
   }
+
+  addElement() {
+    const newLista = this.state.listacomps.concat(
+      <ComponenteListaClase
+        texto={this.valorTextInput.current.value}
+        prioridad={this.valorPrioritySelect.current.value}
+      />
+    );
+    this.setState({ listacomps: newLista });
+  }
+
   render() {
     return (
       <div>
         {this.titulo} - {this.icono}
+        <ul>
+          {this.state.listacomps}
+          <li>
+            <input
+              ref={this.valorTextInput}
+              type="text"
+              placeholder="Introduce una pieza"
+            />
+            <br />
+            <select ref={this.valorPrioritySelect}>
+              <option value="alta">Prioridad Alta</option>
+              <option value="media">Prioridad Media</option>
+              <option value="baja">Prioridad Baja</option>
+            </select>
+            <button onClick={this.addElement.bind(this)}>añadir</button>
+          </li>
+        </ul>
       </div>
     );
   }
